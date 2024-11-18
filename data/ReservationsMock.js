b5c1d99231b13a193d5bddca44700700e4d3ec04
victorien kenfack <kenfackvictorien@gmail.com>
2024-11-17 19:48:24 -0500

    ajout des filtre sur les routes pour faciliter les recherche

---
+++ data/ReservationsMock.js
@@ -0,0 +1,7 @@
+const ReservationsMock = [
+    { reservation_id: 1, livre_id: 1, membre_id: 1, date_reservation: '2023-10-20', statut: 'En attente' },
+    { reservation_id: 2, livre_id: 2, membre_id: 2, date_reservation: '2023-10-21', statut: 'Confirmée' },
+  ];
+  
+  export default ReservationsMock;
+  
--- routes/EmpruntRoutes.js
+++ routes/EmpruntRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllEmprunts, createEmprunt, updateEmprunt, deleteEmprunt } from '../controllers/EmpruntController.js';
+import { getAllEmprunts, createEmprunt, updateEmprunt, deleteEmprunt, getFilteredEmprunts } from '../controllers/EmpruntController.js';
 
 const router = express.Router();
 
 router.get('/', getAllEmprunts); // Obtenir tous les emprunts
+router.get('/filter', getFilteredEmprunts); // Filtrer les emprunts
 router.post('/', createEmprunt); // Ajouter un emprunt
 router.put('/:id', updateEmprunt); // Modifier un emprunt
 router.delete('/:id', deleteEmprunt); // Supprimer un emprunt
