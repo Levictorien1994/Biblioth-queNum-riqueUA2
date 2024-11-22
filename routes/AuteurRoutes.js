52928c4319a7371eddb1c88a60855fbad1c65d84
victorien kenfack <kenfackvictorien@gmail.com>
2024-11-22 18:49:33 -0500

    ajout des autorisation pour la creation des livres

--- routes/AuteurRoutes.js
+++ routes/AuteurRoutes.js
@@ -6,14 +6,14 @@ import {
   updateAuteur,
   deleteAuteur,
 } from '../controllers/AuteurController.js';
-
+import { authenticateToken, authorizeRole } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 // Routes
-router.get('/', getAllAuteurs); // Obtenir tous les auteurs
-router.get('/:id', getAuteurById); // Obtenir un auteur par ID
-router.post('/', createAuteur); // Ajouter un nouvel auteur
-router.put('/:id', updateAuteur); // Mettre à jour un auteur
-router.delete('/:id', deleteAuteur); // Supprimer un auteur
+router.get('/', authenticateToken,getAllAuteurs); // Obtenir tous les auteurs
+router.get('/:id', authenticateToken,getAuteurById); // Obtenir un auteur par ID
+router.post('/',authenticateToken, createAuteur); // Ajouter un nouvel auteur
+router.put('/:id', authenticateToken,updateAuteur); // Mettre à jour un auteur
+router.delete('/:id',authenticateToken, deleteAuteur); // Supprimer un auteur
 
 export default router;
