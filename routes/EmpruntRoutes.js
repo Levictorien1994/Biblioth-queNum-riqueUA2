0b5c08ec2c02d2047e0762cc3f33c5e9062b1fc0
Erick Bergevin Donfack Tsagueu <2728586@collegelacite.ca>
2024-11-23 22:35:43 -0500

    Ajout des validations et des authentifications

--- routes/EmpruntRoutes.js
+++ routes/EmpruntRoutes.js
@@ -12,10 +12,10 @@ import {
 const router = express.Router();
 
 // Routes
-router.get('/',authenticateToken, getAllEmprunts); // Obtenir tous les emprunts
-router.get('/:id', getEmpruntById); // Obtenir un emprunt par ID
-router.post('/',validateEmprunt,authenticateToken,createEmprunt); // Ajouter un emprunt
-router.put('/:id',validateEmprunt,authenticateToken, updateEmprunt); // Mettre à jour un emprunt
-router.delete('/:id',authenticateToken,authorizeRole(['SuperAdmin','Administrateur']), deleteEmprunt); // Supprimer un emprunt
+router.get('/', authenticateToken, getAllEmprunts); // Obtenir tous les emprunts
+router.get('/:id', authenticateToken, getEmpruntById); // Obtenir un emprunt par ID
+router.post('/', validateEmprunt, authenticateToken, createEmprunt); // Ajouter un emprunt
+router.put('/:id', validateEmprunt, authenticateToken, updateEmprunt); // Mettre à jour un emprunt
+router.delete('/:id', authenticateToken, authorizeRole(['SuperAdmin', 'Administrateur']), deleteEmprunt); // Supprimer un emprunt
 
 export default router;
--- routes/UtilisateurRoutes.js
+++ routes/UtilisateurRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { validateUtilisateur, validateLoginUtilisateur } from '../middlewares/validationMiddleware.js';
-import{authenticateToken,authorizeRole} from '../middlewares/authMiddleware.js'
+import { authenticateToken, authorizeRole } from '../middlewares/authMiddleware.js'
 import {
   getAllUtilisateurs,
   getUtilisateurById,
@@ -13,12 +13,11 @@ import {
 const router = express.Router();
 
 // Routes
-router.get('/', getAllUtilisateurs); // Obtenir tous les utilisateurs
-router.get('/:id', getUtilisateurById); // Obtenir un utilisateur par ID
-//router.post('/', createUtilisateur); // Ajouter un utilisateur
-router.put('/:id', updateUtilisateur); // Mettre à jour un utilisateur
-router.delete('/:id', deleteUtilisateur); // Supprimer un utilisateur
-router.post('/',validateUtilisateur,createUtilisateur);
+router.get('/', authorizeRole(['SuperAdmin']), authenticateToken, getAllUtilisateurs); // Obtenir tous les utilisateurs
+router.get('/:id', authorizeRole(['SuperAdmin']), authenticateToken, getUtilisateurById); // Obtenir un utilisateur par ID
+router.put('/:id', authorizeRole(['SuperAdmin']), authenticateToken, updateUtilisateur); // Mettre à jour un utilisateur
+router.delete('/:id', authorizeRole(['SuperAdmin']), authenticateToken, deleteUtilisateur); // Supprimer un utilisateur
+router.post('/', authorizeRole(['SuperAdmin']), authenticateToken, validateUtilisateur, createUtilisateur);
 router.post('/login', validateLoginUtilisateur, loginUtilisateur);
 
 export default router;
